814f1cf15aeaf2dae1c7c2e1ec66b535b4f85ce6
Onee <adeelkhwarazmi@icloud.com>
2026-05-30 08:27:42 +0100

    added new page

--- static/js/main.js
+++ static/js/main.js
@@ -1,53 +1,83 @@
 document.body.style.backgroundSize = "75%";
 document.body.style.cursor = "url('./static/images/DotCursorShadow.cur'), auto;";
 
-function setActive(logo) {
-  if (logo.alt === "About Me Logo") {
+function setActive(logo) 
+{
+  if (logo.alt === "About Me Logo") 
+  {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "block";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
     document.getElementById("page5").style.display = "none";
     document.getElementById("page6").style.display = "none";
-  } else if (logo.alt === "Home Logo") {
+    document.getElementById("page7").style.display = "none";
+  } 
+  else if (logo.alt === "Home Logo") 
+  {
     document.getElementById("page1").style.display = "block";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
     document.getElementById("page5").style.display = "none";
     document.getElementById("page6").style.display = "none";
-  } else if (logo.alt === "Devils Crown Logo") {
+    document.getElementById("page7").style.display = "none";
+  } 
+  else if (logo.alt === "Devils Crown Logo") 
+  {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "block";
     document.getElementById("page4").style.display = "none";
     document.getElementById("page5").style.display = "none";
     document.getElementById("page6").style.display = "none";
-  } else if (logo.alt === "Blender Logo") {
+    document.getElementById("page7").style.display = "none";
+  } 
+  else if (logo.alt === "Blender Logo") 
+  {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "block";
     document.getElementById("page5").style.display = "none";
     document.getElementById("page6").style.display = "none";
-  } else if (logo.alt === "VOVE Logo") {
+    document.getElementById("page7").style.display = "none";
+  } 
+  else if (logo.alt === "VOVE Logo") 
+  {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
     document.getElementById("page5").style.display = "block";
     document.getElementById("page6").style.display = "none";
-  } else if (logo.alt === "Dead-Sector Logo") {
+    document.getElementById("page7").style.display = "none";
+  } 
+  else if (logo.alt === "Dead-Sector Logo") 
+  {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
     document.getElementById("page5").style.display = "none";
     document.getElementById("page6").style.display = "block";
+    document.getElementById("page7").style.display = "none";
+  }
+  else if (logo.alt === "Nova Logo") 
+  {
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "none";
+    document.getElementById("page6").style.display = "none";
+    document.getElementById("page7").style.display = "block";
   }
 
   const logos = document.querySelectorAll('.logo');
-  logos.forEach(l => {
+
+  logos.forEach(l => 
+  {
     l.classList.remove('active');
   });
 
