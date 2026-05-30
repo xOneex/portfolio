c3bc2d8a2b0022569c00733806eebb6c744ab449
Onee <adeelkhwarazmi@icloud.com>
2026-05-30 06:35:56 +0100

    new page added

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,3 @@
-/*document.body.style.backgroundImage = "url('./static/images/Background.png')";*/
 document.body.style.backgroundSize = "75%";
 document.body.style.cursor = "url('./static/images/DotCursorShadow.cur'), auto;";
 
@@ -8,23 +7,32 @@ function setActive(logo) {
     document.getElementById("page2").style.display = "block";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "none";
   } else if (logo.alt === "Home Logo") {
     document.getElementById("page1").style.display = "block";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "none";
   } else if (logo.alt === "Devils Crown Logo") {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "block";
     document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "none";
   } else if (logo.alt === "Blender Logo") {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
     document.getElementById("page4").style.display = "block";
+    document.getElementById("page5").style.display = "none";
+  } else if (logo.alt === "VOVE Logo") {
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "block";
   }
-
 
   const logos = document.querySelectorAll('.logo');
   logos.forEach(logo => {
